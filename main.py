1846a2547abc0f4a9a5c4ad681687ca56e17ca12
Hansennnn <57466044+Hansennnn@users.noreply.github.com>
2019-12-03 20:46:15 -0800

    Uploading Pseudocode Version
    Note: I indented more in chunks where there were multiple sentences to make it more clear that there were multiple sentences

---
+++ main.py
@@ -0,0 +1,163 @@
+
+                       Pseudocode:
+When game starts:
+   -  Hello World! Welcome to Pixel. 
+
+After game starts:
+   -  Player spawns (you) with no resources except a basic set of tools; pickaxe to break ores; axe to gather wood; a sword to fight creatures, and a small backpack to carry your items
+   
+When in game: 
+   - Use WASD to move and press left click to break objects
+
+After every 20 minutes:
+   - switches time of day
+
+When it's nighttime:
+   - mobs spawn
+
+When you see mobs:
+   - fight or run/seek shelter somewhere
+
+When world loads:
+   - caves also spawn with resources
+
+When you spot a cave:
+   - you can chose to enter to find and collect rare resources; beware of mobs as they'll always be in cave s no matter the time of day
+
+An essential item you must gather:
+   - a larger backpack, which you can forge in order to hold more items
+
+To forge items:
+   - you need the correct materials to make them
+
+To make a house:
+   - craft an axe out of iron, which you can find from caves so you can collect wood
+     - make windows from glass
+
+When game starts:
+   - press "ESC," then click "Log Out," to sign out of the world
+
+In certain caves:
+   - there will be much larger, stronger creatures; like a mini boss fight; drops rare loot to craft magical weapons
+
+Press "CTRL and L" to
+   - display all the possible crafting recipes
+
+Backpack crafting recipe:
+   - Wool (from sheep) + a needle + scissors
+
+Press/Hold "shift" to:
+   - keep a lower profile; ex: hide from mobs
+
+When within 8 meters of a mob:
+   - they will pursue you until you either get out of their sight, or until you die to them, or kill them
+
+When you die:
+   - Game Over; you must restart your world and start fresh; no resources; says "Game Over!" on screen
+
+Game Over:
+   - you have to start a new world; square 1
+
+When you slay an animal:
+   - you can collect the meat with a knife
+
+When eating raw meat:
+   - you can easily get sick
+   
+To cook meat:
+   - light a fire, then put meat on top of it
+
+To make a fire:
+   - gather wood, chop it down into little sticks, then use rocks (which you can find on the ground), and light it with rocks (hitting 2 rocks together automatically lights it on fire in this game)
+
+Player dies if:
+   - they don't eat food after 3 days (120 minutes; 40 mins per day)
+
+Gather water: 
+   - you automatically get one bottle of water per day; the more you move/perform activities, the more water you need to consume; find more water through rain, springs, or plants
+
+When in game:
+   - different biomes with different temperatures corresponding to each biomes
+
+You can craft armor/better weapons by:
+   - gathering the needed ores, then using an anvil, which you can craft through using a lot of iron
+
+In order to walk faster:
+   - double-tap "W" (sprint)
+     
+   - [Object=anything that can be manipulated]
+
+You gain more points as you survive longer:
+   - kill mobs, gather ores, etc
+
+When you launch game:
+   - You can choose between 10 skins, 5 female skins, and 5 male skins, all with varying skin tones, hair styles/color, etc
+
+To open up your backpack to view items:
+   - Press "tab," which opens up a screen that shows everything you currently have in your backpack
+
+In game you have an HP bar:
+   - You have an HP that is 4 inches across your screen
+
+HP bar represents: 
+   - Every quarter of your HP bar (1 inch) represents something;
+     - When HP is 75.1/100 or more (over 3/4), HP bar will be green to represent one being in good health
+       - When HP is 50.1-75%, HP bar will be orange to represent that the body is at a nuetral state; body is low on energy but can still function properly
+         - When HP is 25.1-50%, HP will be yellow to represent one being either sick, lacking food, water, or were injured by a mob/creature
+           - When HP is 0.1/25%, HP will be red, which signifies that one is on the brink of death 
+
+Better weapons...:
+   - More damage; kill things faster
+
+Every mob...:
+   - Has a different amount of HP (even mobs of the same kind; Ex: two zombies might have a different amount of HP, but still have around the same HP assuming both of them have the same amount of armor and are around the same age, etc)
+
+Better armor
+   - More damage can be taken  
+    - Better boots; 
+     - gain +12% movement speed if you have "Mythic Boots," boots that can be forged from an ore gathered from the Golem's Den
+       - Golem's Den = A rare lair that can be found in caves
+        - Golem = Strong creature where you can gather Mythical resources from destroying/defeating item
+
+Running low on food/water (dehydration/malnutrition)
+   - Move slower; can't perform tasks at the usual level
+
+Barbarian's Chestplate:
+   - Along with the  "Mythic Boots," you can also craft the  "Barbarian's Chestplate" with the same material
+     - When equiped, you gain +12% damage, meaning every swing with a weapon does an addition 12% damage (1.12) 
+       - Additionally, rather than taking 50% reduced damage, you take 65% reduced damage from attacks
+
+Basic Armor
+   - Crafted from Iron; allows you to take 50% reduced damage (excluding fall damage)
+
+Water
+   - Player also dies if they don't consume *enough* water
+      - Water intake needed depends on how much work/tasks the player performs
+
+Lava
+   - There can be puddles of lava found in random places
+
+Animals
+   You can find chickens, pigs, cows, birds, boars, 
+
+Sleep
+    Press "alt+s" to make your character sleep; if your character sleeps for 10 seconds without being bothered by anything (mobs, etc), it automatically switches the time of day to daytime
+
+  
+
+
+   
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
